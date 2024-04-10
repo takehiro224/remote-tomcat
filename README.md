3526e301c7dbbcd383c53e987e5917b7bba76f50
takehiro.watanabe <mogu.224@gmail.com>
2024-04-10 09:50:10 +0900

    不要なサンプルファイル削除と検証用のファイルを追加

---
+++ README.md
@@ -0,0 +1,14 @@
+mkdir jsp && mkdir WEB-INF && mkdir -p WEB-INF/classes && mkdir -p WEB-INF/jsp && mkdir -p WEB-INF/lib && mkdir -p WEB-INF/tags && touch WEB-INF/web.xml
+
+project
+project/jsp
+project/META-INF
+project/servlets
+project/WEB-INF
+project/WEB-INF/classes/
+project/WEB-INF/jsp/
+project/WEB-INF/lib/
+project/WEB-INF/tags/
+project/WEB-INF/web.xml
+project/websocket
+project/index.html
---
+++ WEB-INF/classes/HelloServlet.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        try {
+            PrintWriter out = response.getWriter();
+            out.println("<html>");
+            out.println("<head><title>Hello, World!</title></head>");
+            out.println("<body>");
+            out.println("<h1>This is Tomcat web App.</h1>");
+            out.println("</body></html>");
+        } catch (IOException e) {
+            // IOExceptionの処理を行う
+            e.printStackTrace();
+        }
+    }
+}
