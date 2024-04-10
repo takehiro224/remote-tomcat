63fd3e7c95a0734925c78f279fad026f784ad8b7
takehiro.watanabe <mogu.224@gmail.com>
2024-04-10 17:33:28 +0900

    ルーティングを変更

--- WEB-INF/classes/SampleServlet.java
+++ WEB-INF/classes/SampleServlet.java
@@ -6,16 +6,16 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class HelloServlet extends HttpServlet {
+public class SampleServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html");
         try {
             PrintWriter out = response.getWriter();
             out.println("<html>");
-            out.println("<head><title>Hello, World!!!</title></head>");
+            out.println("<head><title>Hello, World!!</title></head>");
             out.println("<body>");
-            out.println("<h1>This is Tomcat Web App.!!!!!!</h1>");
+            out.println("<h1>This is Tomcat Web App from sample.!!!!!!</h1>");
             out.println("</body></html>");
         } catch (IOException e) {
             // IOExceptionの処理を行う
